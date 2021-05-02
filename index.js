d2cded9929ceff50c830575daba30504b074f00b
David Griswold <david.griswold@graded.br>
2021-05-02 10:23:41 -0300

    ready to train!

--- index.js
+++ index.js
@@ -1,31 +1,22 @@
 /// <reference path="global.d.ts" />
 "use strict";
 
-var framenum = 0;
+var circles = [];
+var squares = [];
+var triangles = [];
 
+function preload() {
+  //load in images from the data folder
+
+}
 function setup() {
-  createCanvas(64, 64);
-  textAlign(CENTER, CENTER);
-  rectMode(CENTER);
+  createCanvas(500, 500);
 
 }
 
-/** This function runs in an infinitely repeating loop */
+
 function draw() {
-  background(255);
-  //generate and save random circle
 
-  save(`images/circle${nf(frameCount, 4, 0)}.png`)
-  background(255);
-
-  // generate a random square
-
-  save(`images/square${nf(frameCount, 4, 0)}.png`)
-  background(255);
-  // generate a random triangle
-
-  save(`images/triangle${nf(frameCount, 4, 0)}.png`)
-  background(255);
 
 }
 
